84d546900884fe0880315da26e5b6ffa7b68c59d
Your Name <halelugia96@gmail.com>
2022-12-05 02:04:29 +0700

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Home from './Components/Pages/Home/Home'
+import Login from './Components/State/Login'
 
 function App(props) {
   return (
-    <Home/>
+    <Login/>
   )
 }
 
