c6969f09069073d8aaf709a452643f18c3b51a32
Your Name <halelugia96@gmail.com>
2022-12-12 23:24:11 +0700

    update file user.jsx

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -16,18 +16,18 @@ export class Users extends Component {
     super()
     this.state = {
       userList: [],
-      modalAdd: false,
-      modalEdit: false,
       errors: {
-        nameError: '',
-        emailError: '',
+        name: '',
+        email: '',
       },
-      event: null,
       user: {
         name: '',
         email: '',
         status: true,
       },
+
+      modal: false,
+      event: null,
       currentId: null,
     }
   }
@@ -45,6 +45,7 @@ export class Users extends Component {
     }
   }
 
+  // render list user
   getUser = async () => {
     const res = await fetch(`${SERVER_API}/users`)
     const users = await res.json()
@@ -53,90 +54,6 @@ export class Users extends Component {
       this.setState({
         userList: users,
       })
-    }
-  }
-
-  // dispatch name
-  dispatchName = (event) => {
-    this.setState({
-      event: event,
-    })
-  }
-
-  // dispatch boolean
-  dispatchModalAdd = (state) => {
-    this.setState({
-      modalAdd: state,
-    })
-  }
-
-  showModalAdd = () => {
-    this.dispatchModalAdd(true)
-  }
-
-  hideModalAdd = () => {
-    this.dispatchModalAdd(false)
-    this.setState({
-      errors: {
-        nameError: '',
-        emailError: '',
-      },
-    })
-  }
-
-  dispatchModalEdit = (state) => {
-    this.setState({
-      modalEdit: state,
-    })
-  }
-
-  showModalEdit = () => {
-    this.dispatchModalEdit(true)
-  }
-
-  hideModalEdit = () => {
-    this.dispatchModalEdit(false)
-    this.setState({
-      errors: {
-        nameError: '',
-        emailError: '',
-      },
-    })
-  }
-
-  // * add user
-  // handle form add
-  hanldeFormAddSubmit = (e) => {
-    e.preventDefault()
-
-    const name = e.target.querySelector('[name="name"]').value
-    const email = e.target.querySelector('[name="email"]').value
-    const select = e.target.querySelector('select')
-    const error = {}
-
-    if ((typeof name === 'string') & (name.trim() === '')) {
-      error.nameError = 'Vui lòng nhập tên'
-    }
-    if ((typeof email === 'string') & (email.trim() === '')) {
-      error.emailError = 'Vui lòng nhập email'
-    }
-
-    if (Object.keys(error).length) {
-      this.setState({
-        errors: error,
-      })
-    }
-
-    const status = Number(select.value) === 1 ? true : false
-    const data = {
-      name: name,
-      email: email,
-      status: status,
-    }
-
-    if (Object.keys(error).length === 0) {
-      this.postUser(data)
-      this.hideModalAdd()
     }
   }
 
@@ -156,86 +73,7 @@ export class Users extends Component {
     }
   }
 
-  // * handle change edit value
-  handleChangeName = (e) => {
-    const data = { ...this.state.user }
-    data.name = e.target.value
-    this.setState({
-      user: data,
-    })
-  }
-
-  handleChangeEmail = (e) => {
-    const data = { ...this.state.user }
-    data.email = e.target.value
-    this.setState({
-      user: data,
-    })
-  }
-
-  handleChangeStatus = (e) => {
-    const data = { ...this.state.user }
-    data.status = Number(e.target.value) === 1 ? true : false
-    this.setState({
-      user: data,
-    })
-  }
-
-  // todo edit user
-  // *show modal edit
-  handleShowEdit = (id) => {
-    this.showModalEdit()
-    this.setState({
-      currentId: id,
-    })
-
-    const { userList } = this.state
-    const dataEdit = { ...this.state.user }
-    if (userList.length) {
-      userList.forEach((user) => {
-        if (user.id === id) {
-          dataEdit.name = user.name
-          dataEdit.email = user.email
-          dataEdit.status = user.status
-        }
-      })
-    }
-
-    this.setState({
-      user: dataEdit,
-    })
-  }
-
-  // *handle submit form edit
-  handleFormEditUser = (e) => {
-    e.preventDefault()
-
-    const name = e.target.querySelector('[name="name"]').value
-    const email = e.target.querySelector('[name="email"]').value
-    const select = e.target.querySelector('select')
-    const error = {}
-
-    if ((typeof name === 'string') & (name.trim() === '')) {
-      error.nameError = 'Vui lòng nhập tên'
-    }
-    if ((typeof email === 'string') & (email.trim() === '')) {
-      error.emailError = 'Vui lòng nhập email'
-    }
-
-    if (Object.keys(error).length) {
-      this.setState({
-        errors: error,
-      })
-    }
-
-    const data = { ...this.state.user }
-    if (Object.keys(error).length === 0) {
-      this.patchUser(data, this.state.currentId)
-      this.hideModalEdit()
-    }
-  }
-
-  // * update user
+  // update user
   patchUser = async (data, id) => {
     const res = await fetch(`${SERVER_API}/users/${id}`, {
       method: 'PATCH',
@@ -251,9 +89,114 @@ export class Users extends Component {
     }
   }
 
+  // dispatch name
+  dispatchName = (event) => {
+    this.setState({
+      event: event,
+    })
+  }
+
+  // * dispatch boolean modal
+  dispatchModal = (state) => {
+    this.setState({
+      modal: state,
+    })
+  }
+
+  showModal = (state) => {
+    this.dispatchModal(state)
+  }
+
+  hideModal = (state) => {
+    this.dispatchModal(state)
+    this.setState({
+      errors: {
+        name: '',
+        email: '',
+      },
+
+      user: {
+        name: '',
+        email: '',
+        status: true,
+      },
+
+      currentId: null,
+    })
+  }
+
+  // * handle change value
+  handleChange = (name) => {
+    const data = { ...this.state.user }
+    const value = document.querySelector(`#form [name=${name}]`).value
+
+    name !== 'status'
+      ? (data[`${name}`] = value)
+      : (data[`${name}`] = Number(value) === 1 ? true : false)
+
+    this.setState({
+      user: data,
+    })
+  }
+
+  // * handle submit form
+  hanldeFormSubmit = (e) => {
+    e.preventDefault()
+    const { name, email } = this.state.user
+    const errors = {}
+
+    if ((typeof name === 'string') & (name.trim() === '')) {
+      errors.name = 'Vui lòng nhập tên'
+    }
+    if ((typeof email === 'string') & (email.trim() === '')) {
+      errors.email = 'Vui lòng nhập email'
+    }
+
+    if (Object.keys(errors).length) {
+      this.setState({
+        errors: errors,
+      })
+    }
+
+    const data = { ...this.state.user }
+
+    if (Object.keys(errors).length === 0) {
+      if (this.state.currentId === null) {
+        this.postUser(data)
+      } else {
+        this.patchUser(data, this.state.currentId)
+      }
+
+      this.hideModal(false)
+    }
+  }
+
+  // *show modal edit
+  handleShowEdit = (id) => {
+    this.showModal(true)
+    this.setState({
+      currentId: id,
+    })
+
+    const { userList } = this.state
+    const data = { ...this.state.user }
+    if (userList.length) {
+      userList.forEach((user) => {
+        if (user.id === id) {
+          data.name = user.name
+          data.email = user.email
+          data.status = user.status
+        }
+      })
+    }
+
+    this.setState({
+      user: data,
+    })
+  }
+
   render() {
-    const { userList, errors, user } = this.state
-    const { nameError, emailError } = errors
+    const { userList, errors, user, currentId } = this.state
     const { name, email, status } = user
 
     return (
@@ -261,7 +204,9 @@ export class Users extends Component {
         <h1 className="my-5">Quản lý người dùng</h1>
 
         <button
-          onClick={this.showModalAdd}
+          onClick={() => {
+            this.showModal(true)
+          }}
           type="button"
           className="mb-3 btn btn-primary"
         >
@@ -332,94 +277,61 @@ export class Users extends Component {
           Xóa đã trọn (0)
         </button>
 
-        <Modal show={this.state.modalAdd} onHide={this.hideModalAdd}>
+        <Modal
+          show={this.state.modal}
+          onHide={() => {
+            this.hideModal(false)
+          }}
+        >
           <Modal.Header closeButton>
-            <Modal.Title>Thêm công việc mới</Modal.Title>
+            <Modal.Title>
+              {!currentId ? 'Thêm công việc mới' : 'Sửa công việc '}
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <form onSubmit={this.hanldeFormAddSubmit} id="addUser">
+            <form onSubmit={this.hanldeFormSubmit} id="form">
               <div className="mb-3">
                 <label htmlFor="">Tên</label>
                 <input
                   type="text"
                   className={
-                    nameError ? 'form-control is-invalid' : 'form-control'
+                    errors.name ? 'form-control is-invalid' : 'form-control'
                   }
-                  placeholder="Tên..."
-                  name="name"
-                />
-                <div className="invalid-feedback">{nameError}</div>
-              </div>
-
-              <div className="mb-3">
-                <label htmlFor="">Email</label>
-                <input
-                  type="email"
-                  className={
-                    emailError ? 'form-control is-invalid' : 'form-control'
-                  }
-                  placeholder="Email..."
-                  name="email"
-                />
-                <div className="invalid-feedback">{emailError}</div>
-              </div>
-
-              <div className="mb-3">
-                <soan>Trạng thái</soan>
-                <select className="form-select">
-                  <option value="1">Kích hoạt</option>
-                  <option value="0">Chưa kích hoạt</option>
-                </select>
-              </div>
-
-              <button type="submit" className="btn btn-success">
-                Lưu
-              </button>
-            </form>
-          </Modal.Body>
-        </Modal>
-
-        <Modal show={this.state.modalEdit} onHide={this.hideModalEdit}>
-          <Modal.Header closeButton>
-            <Modal.Title>Sửa công việc</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <form id="editUser" onSubmit={this.handleFormEditUser}>
-              <div className="mb-3">
-                <label htmlFor="">Tên</label>
-                <input
-                  className={
-                    nameError ? 'form-control is-invalid' : 'form-control'
-                  }
-                  type="text"
                   placeholder="Tên..."
                   name="name"
                   value={name}
-                  onChange={this.handleChangeName}
+                  onChange={() => {
+                    this.handleChange('name')
+                  }}
                 />
-                <div className="invalid-feedback">{nameError}</div>
+                <div className="invalid-feedback">{errors.name}</div>
               </div>
 
               <div className="mb-3">
                 <label htmlFor="">Email</label>
                 <input
-                  className={
-                    emailError ? 'form-control is-invalid' : 'form-control'
-                  }
                   type="email"
+                  className={
+                    errors.email ? 'form-control is-invalid' : 'form-control'
+                  }
                   placeholder="Email..."
                   name="email"
                   value={email}
-                  onChange={this.handleChangeEmail}
+                  onChange={() => {
+                    this.handleChange('email')
+                  }}
                 />
-                <div className="invalid-feedback">{emailError}</div>
+                <div className="invalid-feedback">{errors.email}</div>
               </div>
 
               <div className="mb-3">
-                <soan>Trạng thái</soan>
+                <span>Trạng thái</span>
                 <select
                   className="form-select"
-                  onChange={this.handleChangeStatus}
+                  onChange={() => {
+                    this.handleChange('status')
+                  }}
+                  name="status"
                 >
                   {status ? (
                     <option selected value="1">
